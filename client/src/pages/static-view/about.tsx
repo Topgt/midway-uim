367e4c733ac13341de9b44fb1dc24509d21178ec
兰哒 <topgt1@163.com>
2020-04-19 16:15:01 +0800

    style: 修改关于作者页面的样式

--- client/src/pages/static-view/about.tsx
+++ client/src/pages/static-view/about.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'dva';
-import { Button } from 'antd'
+import { Button, Spin } from 'antd'
 import HeaderNav from './components/header-nav'
 import Markdow from '../markdown/show'
 import Footer from './components/footer'
@@ -21,9 +21,13 @@ function About ({fileContent, dispatch}:any) {
         activetI="activet4"
       />
       <div style={{height: '50px'}}/>
-      <article className="article">
-        <Markdow content={fileContent} />
-        <Button onClick={downFile} type="primary" icon="download" style={{display:'block', margin: '15px auto 0 auto'}}>
+      <article className="article" style={{minHeight: 'calc(100% - 365px)', display: 'flex', flexDirection: 'column'}}>
+        {
+          (!fileContent )
+            ? (<div style={{flex: 1,display: 'flex', justifyContent: 'center'}}><Spin><i /></Spin></div>)
+            : <Markdow content={fileContent} />
+        }
+        <Button disabled={!fileContent} onClick={downFile} type="primary" icon="download" style={{display:'block', margin: '15px auto 0 auto'}}>
           下载该简历
         </Button>
       </article>
